430a942263f2ce8e60c7808b30dcd33a46d91cfd
Stanisław Szymczyk <sszymczy@gmail.com>
2025-01-15 15:47:01 +0100

    Modified computed metrics so that there are separate columns for each problem size.

--- plot_graph.py
+++ plot_graph.py
@@ -5,6 +5,7 @@ import argparse
 import matplotlib.ticker as tck
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-o", "--output", help="Write rendered plot to this file.")
@@ -14,15 +15,18 @@ output_file = args.output
 
 df = pd.read_csv(sys.stdin)
 
-# Sort the DataFrame by model name and problem size for consistent plotting
-df = df.sort_values(by=['model_name', 'problem_size'])
+col_names = df.columns[3:].values
+problem_sizes = list(map(lambda c: int(c.removeprefix('lineage-')), col_names))
 
 # Plot the data
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(12, 8))
 
-# Group by model name and plot each group
-for model_name, group in df.groupby('model_name'):
-    plt.plot(group['problem_size'], group['lineage'], marker='o', label=model_name)
+colormap = plt.cm.nipy_spectral
+colors = colormap(np.linspace(0, 1, len(df['model_name'])))
+plt.gca().set_prop_cycle('color', colors)
+
+for index, row in df.iterrows():
+    plt.plot(problem_sizes, row[col_names], marker='o', label=row['model_name'])
 
 # Customize the plot
 plt.title('Lineage benchmark scores for different problem sizes.')
