5e745e12f00ce5ed1ecea4534cecb22ada770e1b
Stanisław Szymczyk <sszymczy@gmail.com>
2025-11-22 18:01:44 +0100

    Added reasoning effort column in results table.

--- compute_metrics.py
+++ compute_metrics.py
@@ -30,6 +30,7 @@ def extract_answer(row, relaxed):
             r'ANSWER>([0-9])</ANSWER>',
             r'<ANSWER>([0-9])\\</ANSWER>',
             r'<ANSWER\\>([0-9])\\</ANSWER\\>',
+            r'<ANSWER>([0-9])</ANSWER',
         ]
         for relaxed_answer_regex in relaxed_answer_regexes:
             matches = re.findall(relaxed_answer_regex, row['model_response'], re.M)
@@ -55,27 +56,28 @@ df['model_answer'] = df.apply(extract_answer, axis=1, args=(is_answer_relaxed,))
 df['answer_correct'] = df['correct_answer'] == df['model_answer']
 df['answer_incorrect'] = (df['correct_answer'] != df['model_answer']) & (0 != df['model_answer'])
 df['answer_missing'] = 0 == df['model_answer']
+df['reasoning_effort'] = df['reasoning_effort'].fillna("N/A")
 
-df = df[['problem_size', 'relation_name', 'model_name', 'answer_correct', 'answer_incorrect', 'answer_missing']]
+df = df[['problem_size', 'relation_name', 'model_name', 'reasoning_effort', 'answer_correct', 'answer_incorrect', 'answer_missing']]
 
 if is_output_detailed:
-    df = df.groupby(['problem_size', 'relation_name', 'model_name'])[['answer_correct', 'answer_incorrect', 'answer_missing']].sum().reset_index()
+    df = df.groupby(['problem_size', 'relation_name', 'model_name', 'reasoning_effort'])[['answer_correct', 'answer_incorrect', 'answer_missing']].sum().reset_index()
 else:
-    df = df.groupby(['problem_size', 'relation_name', 'model_name'])['answer_correct'].mean().reset_index().rename(columns={'answer_correct': 'percent_correct'})
+    df = df.groupby(['problem_size', 'relation_name', 'model_name', 'reasoning_effort'])['answer_correct'].mean().reset_index().rename(columns={'answer_correct': 'percent_correct'})
 
-    df = df.pivot(index=['problem_size', 'model_name'], columns='relation_name', values='percent_correct').reset_index()
+    df = df.pivot(index=['problem_size', 'model_name', 'reasoning_effort'], columns='relation_name', values='percent_correct').reset_index()
 
     df['lineage'] = df[['ANCESTOR', 'DESCENDANT', 'COMMON_ANCESTOR', 'COMMON_DESCENDANT']].mean(axis=1)
 
-    df = df[['problem_size', 'model_name', 'lineage']]
+    df = df[['problem_size', 'model_name', 'reasoning_effort', 'lineage']]
 
-    df = df.pivot(index=['model_name'], columns='problem_size', values='lineage').fillna(0).reset_index()
+    problem_sizes = sorted(df['problem_size'].unique())
 
-    problem_sizes = [8, 16, 32, 64]
+    df = df.pivot(index=['model_name', 'reasoning_effort'], columns='problem_size', values='lineage').fillna(0).reset_index()
 
     df['lineage'] = df[problem_sizes].mean(axis=1)
 
-    df = df[['model_name', 'lineage'] + problem_sizes]
+    df = df[['model_name', 'reasoning_effort', 'lineage'] + problem_sizes]
 
     df = df.rename(columns={ size: f'lineage-{size}' for size in problem_sizes })
 
@@ -83,7 +85,7 @@ else:
 
     df['Nr'] = df['lineage'].rank(method='min', ascending=False).astype('int32')
 
-    df = df[['Nr', 'model_name', 'lineage'] + [f'lineage-{size}' for size in problem_sizes]]
+    df = df[['Nr', 'model_name', 'reasoning_effort', 'lineage'] + [f'lineage-{size}' for size in problem_sizes]]
 
 if gen_csv:
     print(df.to_csv(index=False))
